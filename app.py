0cfbcba732160850e932b791907052c55a036f31
tharinda <tharinda221@gmail.com>
2015-12-29 14:54:07 +0530

    facebook api created

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 __author__ = 'tharinda'
 
-
-from jaxRS.index import *
 import flask
 from flask_restful import Api
 
-
+from jaxRS.index import *
+from backend.facebook import *
+from jaxRS.facebook import *
 
 app = flask.Flask(__name__)
 api = Api(app)
 
-
 api.add_resource(Main, '/')
-
-
+api.add_resource(authorize_facebook, '/authorize')
+api.add_resource(handle_callback, '/callback')
+api.add_resource(user_details, '/userDetails')
+#api.add_resource(facebook, '/social/<string:provider>', endpoint = 'social')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)
