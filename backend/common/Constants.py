12f30fb8886c5be254fa8314f90fe244a63a96e0
tharinda <tharinda221@gmail.com>
2016-01-14 01:09:09 +0530

    Finalized for the first release

--- backend/common/Constants.py
+++ backend/common/Constants.py
@@ -26,7 +26,8 @@ class facebookConstants:
     secretKey = "810caf413c4786364eb65172fed5a03c"
     appID = "1672045909732059"
     GraphAPIVersion = "v2.5"
-    redirectURL = "http://" + host + ":" + str(port) + "/callback/facebook"
+    # redirectURL = "http://" + host + ":" + str(port) + "/callback/facebook"
+    redirectURL = "http://tharinda221.pythonanywhere.com/callback/facebook"
     baseGraphApiUrl = "https://graph.facebook.com/v2.5/"
     getUserInitInfoUrl = "me?fields=name,birthday,about,bio,email,education,gender," \
                          "id,hometown"
