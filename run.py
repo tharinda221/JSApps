51b1f78d4ca84b7e11ea1140636da627b9bd2208
tharinda <tharinda221@gmail.com>
2016-02-13 14:45:08 +0530

    setup for the first release

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from config import *
 from JSApps import *
 from backend.database.Operations import *
 if __name__ == '__main__':
-    # app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(host=host, port=port, debug=True, threaded=True)
